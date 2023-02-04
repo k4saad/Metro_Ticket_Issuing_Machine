7312cb4d5a41a5b6bfe0d6a2f58e89dd083b946f
saad khan <saadkhan108b@gmail.com>
2023-02-04 13:16:36 +0530

    added DBAbstraction header and source file

---
+++ Metro_Ticket_Issuing_Machine/DBAbstraction.h
@@ -0,0 +1,19 @@
+#pragma once
+#include<string>
+#include"sqlite3.h"
+using namespace std;
+
+class DBAbstraction {
+
+public:
+	DBAbstraction(string pathToFile);
+	~DBAbstraction();
+
+protected:
+	sqlite3* db;
+
+	bool executeQueryNoResultsBack(sqlite3_stmt* myStatement);
+	bool executeQueryNoResultsBack(string sql);
+	bool prepareQueryWithResults(string sql, sqlite3_stmt*& myStatement);
+
+};
